86f1937e4de3591ef43ecc80c62b1fb614ecf96d
tonycatgogogo <gonghaitao1@gmail.com>
2019-04-22 15:24:16 +0800

    2019.4.22 by tonyCat

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,104 @@
 //index.js
-//获取应用实例
-const app = getApp()
-
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        bannerList: [],
+        navList: []
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        // 1.请求轮播图数据
+        const list = [{
+            "id": 3,
+            "image": "https://image1.suning.cn/uimg/cms/img/155572257063879778.gif?format=_is_1242w_610h",
+            "link": ""
+        },
+            {
+                "id": 4,
+                "image": "https://image.suning.cn/uimg/aps/material/155566632491683107.jpg?format=_is_1242w_610h",
+                "link": ""
+            },
+            {
+                "id": 5,
+                "image": "https://image.suning.cn/uimg/aps/material/155555971120765914.jpg?format=_is_1242w_610h",
+                "link": ""
+            }
+        ]
+        wx.request({
+            url: "https://locally.uieee.com/slides",
+            success: (res) => {
+                list.forEach(item => {
+                    res.data.push(item)
+                })
         this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+            bannerList: res.data
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+        })
+        // 2.请求导航栏数据
+        wx.request({
+            url: "https://locally.uieee.com/categories",
+            success: (res) => {
+                this.setData({
+                    navList: res.data
+                })
+            }
+        })
+
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
   }
 })
